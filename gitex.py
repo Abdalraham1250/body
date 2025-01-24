5f0a7a2d2587d1b4a80fb7f7cb6a22dd020cdbad
abdarahman <body12500@gmail.com>
2025-01-24 09:34:28 +0300

    python file has added

---
+++ gitex.py
@@ -0,0 +1,9 @@
+#get commands :
+    #git clone {url of the repository}
+    #ls   #short of list # to show a list of folders in the big folder
+    #cd   #short of change directory #change the folder you in, to inner folder
+    #touch {name}  #to make a file in the folder #don't work in windows 
+    #git add {file name} # to put the file in a staging area (an area have the files that you added)
+    #git commit -m "commit message" #to save the files in staging area in the repository  #the commit message is to explave the changes that you have made
+    #git config --global user.name "Your Name" #to save the name of who make the changes
+    #git config --global user.email "your_email@example.com" #to save the email of who make the changes
